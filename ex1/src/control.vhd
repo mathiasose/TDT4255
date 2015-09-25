9172b1b0f2ee6fb842f10535937d07ea30ea3670
Jonas Halland <jonashalland@outlook.com>
2015-09-25 14:42:05 +0200

    started implementing things

---
+++ ex1/src/control.vhd
@@ -0,0 +1,62 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use defs.all;
+
+entity control is
+	type (STALL, FETCH, EXECUTE, LOAD_STORE) is state_t;
+	type (ADD, SUB, SLT, ALU_AND, ALU_OR) is alu_t;
+	signal state, next_state: state_t;
+	port(
+		clk : in std_logic;
+		reset : in std_logic;	
+		instruction : in instruction_t;
+		RegDst : out std_logic;
+		Branch : out std_logic;
+		MemRead : out std_logic;
+		MemtoReg : out std_logic;
+		ALUOp : out alu_operation_t;
+		MemWrite : out std_logic;
+		ALUSrc : out std_logic;
+		RegWrite : out std_logic
+	);
+end control;
+
+architecture Behavioral of control is
+	signal opcode : std_logic_vector(5 downto 0);
+begin
+	opcode <= instruction(31 downto 26);
+	with state select
+	-- State transitions
+	next_state <=
+		FETCH when STALL,
+		EXECUTE when FETCH;
+		
+	DrivingOutputs : process(state) is
+	begin
+		-- Setting defaults to avoid latches
+		RegDst <= '0';
+		Branch <= '0';
+		MemRead <= '0';
+		MemtoReg <= '0';
+		AluOp <= (others => '0');
+		MemWrite <= '0';
+		AluSrc <= '0';
+		RegWrite <= '0';
+		
+		-- Non default outputs
+		case state is
+			when EXECUTE =>
+		end case;
+	end process DrivingOutputs;
+	
+	process (clk, reset) is
+	begin
+		if reset = '1' then
+			state <= STALL;
+		elsif rising_edge(clk) then
+			state <= next_state;
+		end if;
+	end process;
+end Behavioral;
+
---
+++ ex1/src/defs.vhd
@@ -0,0 +1,8 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+
+package defs is
+	subtype instruction_t is std_logic_vector(31 downto 0);
+	type alu_operation_t is (ADD, SUB, SLT, ALU_AND, ALU_OR);
+	type state_t is (STALL, FETCH, EXECUTE, LOAD_STORE);
+	subtype opcode_t is std_logic_vector(5 downto 0);
