78692330a00948a575cee7fcf0290ca503f661a0
Mathias Ose <mathias.ose@gmail.com>
2015-09-27 15:12:23 +0200

    Start implementing program counter, with testbench
    Passes simple test of reset and clock increment

---
+++ ex1/src/PC.vhd
@@ -0,0 +1,39 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity PC is
+    Port (
+        clk : in  STD_LOGIC
+        ; rst : in  STD_LOGIC := '0'
+        ; instr_in : in  STD_LOGIC_VECTOR(31 downto 0) := (others => '0')
+        ; jump : in  STD_LOGIC := '0'
+        ; branch : in STD_LOGIC := '0'
+        ; alu_zero : in STD_LOGIC := '0'
+        ; addr_out : out  STD_LOGIC_VECTOR(31 downto 0)
+    );
+end PC;
+
+architecture Behavioral of PC is
+    signal pc : STD_LOGIC_VECTOR(31 downto 0) := (others => '0'); -- register
+    signal jump_addr : STD_LOGIC_VECTOR(31 downto 0); -- bus
+    signal imm_value : STD_LOGIC_VECTOR(15 downto 0); -- bus
+    signal imm_value_extended : STD_LOGIC_VECTOR(31 downto 0); --bus
+begin
+    process(clk, rst)
+	begin
+		if rst = '1' then
+			pc <= (others => '0');
+		elsif rising_edge(clk) then
+			if jump = '1' then
+                --pc <= jump_addr;
+            else
+                pc <= STD_LOGIC_VECTOR(unsigned(pc) + 1);
+            end if;
+		end if;
+	end process;
+    
+    --jump_addr <= pc(31 downto 26) & instr_in(25 downto 0);
+    addr_out <= pc;
+end Behavioral;
+
---
+++ ex1/testbench/tb_PC.vhd
@@ -0,0 +1,73 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+USE work.testutil.ALL;
+ 
+ENTITY tb_PC IS
+END tb_PC;
+ 
+ARCHITECTURE behavior OF tb_PC IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT PC
+    PORT(
+         clk : IN  std_logic;
+         rst : IN  std_logic;
+         instr_in : IN  std_logic_vector(31 downto 0);
+         jump : IN  std_logic;
+         addr_out : OUT  std_logic_vector(31 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal clk : std_logic := '0';
+   signal rst : std_logic := '0';
+   signal instr_in : std_logic_vector(31 downto 0) := (others => '0');
+   signal jump : std_logic := '0';
+
+ 	--Outputs
+   signal addr_out : std_logic_vector(31 downto 0);
+
+   -- Clock period definitions
+   constant clk_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: PC PORT MAP (
+          clk => clk,
+          rst => rst,
+          instr_in => instr_in,
+          jump => jump,
+          addr_out => addr_out
+        );
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      wait for clk_period;
+      rst <= '1';
+      wait for clk_period;
+      rst <= '0';
+      check(addr_out = x"00000000", "PC should reset to 0 address");
+
+      wait for clk_period;
+      check(addr_out = x"00000001", "PC should increment by 1");
+
+      report "Test success";
+      wait;
+   end process;
+
+END;
