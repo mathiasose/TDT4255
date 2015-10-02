450d35fe73a28e614b8213589fe6acacbff6d97c
Jonatan Lund <jonatan.lund.92@gmail.com>
2015-10-02 15:29:20 +0200

    Modified registers

--- ex1/src/registers.vhd
+++ ex1/src/registers.vhd
@@ -4,20 +4,33 @@ use IEEE.NUMERIC_STD.ALL;
 use work.defs.all;
 
 entity registers is
-    Port ( read_register_1 : in  STD_LOGIC_VECTOR (5 downto 0);
-           read_register_2 : in  STD_LOGIC_VECTOR (5 downto 0);
-           write_register : in  STD_LOGIC_VECTOR (3 downto 0);
-           write_data : in  STD_LOGIC_VECTOR (7 downto 0);
-           read_data_1 : out  STD_LOGIC_VECTOR (31 downto 0);
-           read_data_2 : out  STD_LOGIC_VECTOR (31 downto 0);
-           register_write : in  STD_LOGIC);
+    Port ( 
+		clock : in STD_LOGIC;
+		read_register_1 : in  STD_LOGIC_VECTOR (5 downto 0);
+		read_register_2 : in  STD_LOGIC_VECTOR (5 downto 0);
+		write_register : in  STD_LOGIC_VECTOR (5 downto 0);
+		write_data : in  STD_LOGIC_VECTOR (31 downto 0);
+		read_data_1 : out  STD_LOGIC_VECTOR (31 downto 0);
+		read_data_2 : out  STD_LOGIC_VECTOR (31 downto 0);
+		register_write : in  STD_LOGIC);
 end registers;
 
 architecture Behavioral of registers is
-
-	signal
+	type registerArray is array(0 to 31) of STD_LOGIC_VECTOR(31 downto 0);
+	signal registers : registerArray;
 begin
-
-
+	process(clock) is
+	begin
+	
+		if rising_edge(clock) then
+			if register_write = '1' then
+				registers(to_integer(unsigned(write_register))) <= write_data;
+			end if;
+			
+			read_data_1 <= registers(to_integer(unsigned(read_register_1)));
+			read_data_2 <= registers(to_integer(unsigned(read_register_2)));
+		end if;
+		
+	end process;
 end Behavioral;
 
