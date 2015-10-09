49a5288d61fe27a8784ebde6de5a47077507b168
Mathias Ose <mathias.ose@gmail.com>
2015-10-09 13:33:06 +0200

    Fix cases, indentation, add dummy test for control

--- ex1/src/control.vhd
+++ ex1/src/control.vhd
@@ -4,74 +4,76 @@ use IEEE.NUMERIC_STD.ALL;
 use work.defs.all;
 
 entity control is
-		port(
-		clk : in std_logic;
-		reset : in std_logic;	
-		instruction : in instruction_t;
-		RegDst : out std_logic;
-		Branch : out std_logic;
-		MemRead : out std_logic;
-		MemtoReg : out std_logic;
-		ALUOp : out alu_operation_t;
-		MemWrite : out std_logic;
-		ALUSrc : out std_logic;
-		RegWrite : out std_logic
-	);
+    port(
+        clk : in std_logic;
+        reset : in std_logic;
+        instruction : in instruction_t;
+        RegDst : out std_logic;
+        Branch : out std_logic;
+        MemRead : out std_logic;
+        MemtoReg : out std_logic;
+        ALUOp : out alu_operation_t;
+        MemWrite : out std_logic;
+        ALUSrc : out std_logic;
+        RegWrite : out std_logic
+    );
 end control;
 
 architecture Behavioral of control is
-	signal opcode : opcode_t;
-	signal funct : std_logic_vector(5 downto 0);
-	signal state, next_state: state_t;
+    signal opcode : opcode_t;
+    signal funct : std_logic_vector(5 downto 0);
+    signal state, next_state: state_t;
 begin
-	opcode <= instruction(31 downto 26);
-	opcode <= instruction(5 downto 0);
-	
-	with state select
-	-- State transitions
-	next_state <=
-		FETCH when STALL,
-		EXECUTE when FETCH;
-		
-	DrivingOutputs : process(state) is
-	begin
-		-- Setting defaults to avoid latches
-		RegDst <= '0';
-		Branch <= '0';
-		MemRead <= '0';
-		MemtoReg <= '0';
-		AluOp <= NO_OP;
-		MemWrite <= '0';
-		AluSrc <= '0';
-		RegWrite <= '0';
-		
-		-- Non default outputs
-		case state is
-			when EXECUTE =>
-				case funct is
-					when "100000" =>
-						AluOp <= ADD;
-					when "100010" =>
-						AluOp <= ALU_AND;
-					when "100010" =>
-						AluOp <= SUB;
-					when "101010" =>
-						AluOp <= SLT;
-					when "100101" =>
-						AluOp <= ALU_OR;
-					when others => AluOp <= NO_OP;
-				end case;
-		end case;
-		
-	end process DrivingOutputs;
-	
-	process (clk, reset) is
-	begin
-		if reset = '1' then
-			state <= STALL;
-		elsif rising_edge(clk) then
-			state <= next_state;
-		end if;
-	end process;
+    opcode <= instruction(31 downto 26);
+    opcode <= instruction(5 downto 0);
+
+    with state select
+    -- State transitions
+    next_state <=
+        FETCH when STALL,
+        EXECUTE when FETCH,
+        STALL when others;
+
+    DrivingOutputs : process(state) is
+    begin
+        -- Setting defaults to avoid latches
+        RegDst <= '0';
+        Branch <= '0';
+        MemRead <= '0';
+        MemtoReg <= '0';
+        AluOp <= NO_OP;
+        MemWrite <= '0';
+        AluSrc <= '0';
+        RegWrite <= '0';
+
+        -- Non default outputs
+        case state is
+            when EXECUTE =>
+                case funct is
+                    when "100000" =>
+                        AluOp <= ADD;
+                    when "100010" =>
+                        AluOp <= SUB;
+                    when "100100" =>
+                        AluOp <= ALU_AND;
+                    when "100101" =>
+                        AluOp <= ALU_OR;
+                    when "101010" =>
+                        AluOp <= SLT;
+                    when others => AluOp <= NO_OP;
+                end case;
+            when others => AluOp <= NO_OP;
+        end case;
+
+    end process DrivingOutputs;
+
+    process (clk, reset) is
+    begin
+        if reset = '1' then
+            state <= STALL;
+        elsif rising_edge(clk) then
+            state <= next_state;
+        end if;
+    end process;
 end Behavioral;
 
---
+++ ex1/testbench/tb_control.vhd
@@ -0,0 +1,91 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+use work.defs.all;
+use work.testutil.all;
+ 
+ENTITY tb_control IS
+END tb_control;
+ 
+ARCHITECTURE behavior OF tb_control IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT control
+    port(
+        clk : in std_logic;
+        reset : in std_logic;
+        instruction : in instruction_t;
+        RegDst : out std_logic;
+        Branch : out std_logic;
+        MemRead : out std_logic;
+        MemtoReg : out std_logic;
+        ALUOp : out alu_operation_t;
+        MemWrite : out std_logic;
+        ALUSrc : out std_logic;
+        RegWrite : out std_logic
+    );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal clk : std_logic := '0';
+   signal reset : std_logic := '0';
+   signal instruction : std_logic_vector(31 downto 0) := (others => '0');
+
+     --Outputs
+   signal RegDst : std_logic;
+   signal Branch : std_logic;
+   signal MemRead : std_logic;
+   signal MemtoReg : std_logic;
+   signal ALUOp : alu_operation_t;
+   signal MemWrite : std_logic;
+   signal ALUSrc : std_logic;
+   signal RegWrite : std_logic;
+
+   -- Clock period definitions
+   constant clk_period : time := 10 ns;
+
+BEGIN
+
+    -- Instantiate the Unit Under Test (UUT)
+   uut: control PORT MAP (
+          clk => clk,
+          reset => reset,
+          instruction => instruction,
+          RegDst => RegDst,
+          Branch => Branch,
+          MemRead => MemRead,
+          MemtoReg => MemtoReg,
+          ALUOp => ALUOp,
+          MemWrite => MemWrite,
+          ALUSrc => ALUSrc,
+          RegWrite => RegWrite
+        );
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+        clk <= '0';
+        wait for clk_period/2;
+        clk <= '1';
+        wait for clk_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin
+      -- hold reset state for 100 ns.
+      wait for 100 ns;    
+
+      wait for clk_period*10;
+
+      check(1 = 1, "dummy test");
+
+      report "ALL TESTS SUCCESSFUL";
+
+      wait;
+   end process;
+
+END;
