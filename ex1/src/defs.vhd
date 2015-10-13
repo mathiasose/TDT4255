895833866fba530c79cfac085e66d7965ad11438
Mathias Ose <mathias.ose@gmail.com>
2015-10-13 16:13:37 +0200

    Create and test submodule for imm value transforms
    - Changes 16 bit values to 32 bits
    - Supports extend and shift

--- ex1/src/defs.vhd
+++ ex1/src/defs.vhd
@@ -4,10 +4,13 @@ use IEEE.STD_LOGIC_1164.all;
 package defs is
     subtype instruction_t is std_logic_vector(31 downto 0);
     subtype operand_t is std_logic_vector(31 downto 0);
-    type alu_operation_t is (ADD, SUB, SLT, ALU_AND, ALU_OR, NO_OP);
-    type state_t is (STALL, FETCH, EXECUTE);
+    subtype immediate_value_t is std_logic_vector(15 downto 0);
     subtype register_address_t is std_logic_vector(4 downto 0);
     subtype opcode_t is std_logic_vector(5 downto 0);
+
+    type alu_operation_t is (ADD, SUB, SLT, ALU_AND, ALU_OR, NO_OP);
+    type state_t is (STALL, FETCH, EXECUTE);
+    type immediate_value_transformation_t is (SHIFT_LEFT, SIGN_EXTEND);
 
     constant OPERAND_0 : operand_t := x"00000000";
     constant OPERAND_1 : operand_t := x"00000001";
